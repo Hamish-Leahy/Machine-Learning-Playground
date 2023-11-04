deb1ba10c4285b1abd94ccaf8f82c6f80ecc628d
Hamish Leahy <98940688+Hamish-Leahy@users.noreply.github.com>
2023-11-05 09:08:02 +1100

    Hyperparameter optimization scripts

---
+++ Scripts/Hyperparameter Optimization Script.py
@@ -0,0 +1,47 @@
+# Hyperparameter Optimization Script with Grid Search
+
+# Import necessary libraries
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+# Load a sample dataset (Iris dataset)
+data = load_iris()
+X, y = data.data, data.target
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Define the hyperparameter grid
+param_grid = {
+    'n_estimators': [10, 50, 100, 200],
+    'max_depth': [None, 10, 20, 30],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4]
+}
+
+# Create the model (Random Forest Classifier)
+model = RandomForestClassifier(random_state=42)
+
+# Create a GridSearchCV instance
+grid_search = GridSearchCV(model, param_grid, cv=5, scoring='accuracy', n_jobs=-1)
+
+# Perform grid search
+grid_search.fit(X_train, y_train)
+
+# Get the best hyperparameters
+best_params = grid_search.best_params_
+
+# Train a model with the best hyperparameters
+best_model = RandomForestClassifier(random_state=42, **best_params)
+best_model.fit(X_train, y_train)
+
+# Make predictions on the test data
+y_pred = best_model.predict(X_test)
+
+# Calculate accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Best Hyperparameters: {best_params}")
+print(f"Accuracy with Best Hyperparameters: {accuracy:.2f}")
