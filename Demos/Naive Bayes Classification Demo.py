c59ea66806f529a9c44e273d0176e66a4f3d37dd
Hamish Leahy <98940688+Hamish-Leahy@users.noreply.github.com>
2023-11-06 07:11:21 +1100

    Demo Added

---
+++ Demos/Naive Bayes Classification Demo.py
@@ -0,0 +1,16 @@
+# Naive Bayes Classification Demo
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+
+# Generate synthetic data
+X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+y = np.array([1, 1, 1, 2, 2, 2])
+
+# Fit a Gaussian Naive Bayes classifier
+model = GaussianNB()
+model.fit(X, y)
+
+# Make predictions
+new_data = np.array([[-0.8, -1]])
+predicted_class = model.predict(new_data)
+print(f"Predicted class: {predicted_class[0]}")
